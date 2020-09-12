6797989ec171f0b5885953020e11f9e04b7df142
Jorge Chayan <jorgechayan@gmail.com>
2020-09-11 23:51:24 -0500

    Adds test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -76,3 +76,23 @@ RSpec.configure do |config|
   end
 =end
 end
+
+# Trying to debug what's wrong with RSpec and ActionController::Live
+# puts "rspec pid: #{Process.pid}"
+# 
+# trap 'USR1' do
+#   threads = Thread.list
+# 
+#   puts
+#   puts "=" * 80
+#   puts "Received USR1 signal; printing all #{threads.count} thread backtraces."
+# 
+#   threads.each do |thr|
+#     description = thr == Thread.main ? "Main thread" : thr.inspect
+#     puts
+#     puts "#{description} backtrace: "
+#     puts thr.backtrace.join("\n")
+#   end
+# 
+#   puts "=" * 80
+# end
